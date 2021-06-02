af487975e13e487a959fb66a64f30bfdf3de6005
Quentin Le Breton <quentinlebreton@macbook-air-de-quentin.home>
2021-06-02 12:54:55 +0200

    creation des pages + structure bootstrap

--- controller/controller.php
+++ controller/controller.php
@@ -10,10 +10,35 @@ require_once 'model/model.php'; // Charge le fichier model.php
 class Controller {
 	
 	// Ici on appelle la méthode standard des contrôleurs : index
-	public function index($twig) {
+	public function pagetest($twig) {
         $model = new Model(); // Crée un objet Model
         $message = $model->getMessage(); // Appelle la méthode getMessage () du modèle 
         echo $twig->render('pagetest.twig',['message'=>'dgfgdfrtgdtf']);
+    }
+    public function partinscription($twig) {
+        echo $twig->render('partinscription.twig');
+    }
+    public function proinscription($twig) {
+        echo $twig->render('proinscription.twig');
+    }
+    public function kidsinscription($twig) {
+        echo $twig->render('kidsinscription.twig');
+    }
+    public function accueil($twig) {
+        echo $twig->render('accueil.twig');
+    }
+
+    public function moncompte($twig) {
+        echo $twig->render('moncompte.twig');
+    }
+    public function informations($twig) {
+        echo $twig->render('informations.twig');
+    }
+    public function localisation($twig) {
+        echo $twig->render('localisation.twig');
+    }
+    public function financement($twig) {
+        echo $twig->render('financement.twig');
     }
 }
 ?>
--- routes.php
+++ routes.php
@@ -6,5 +6,69 @@ Route::get('/page-test', function(){
     'cache' => false,
     ]);
     $controller = new Controller();
-    $controller->index($twig);
+    $controller->pagetest($twig);
+});
+Route::get('/partinscription', function(){
+    $loader = new \Twig\Loader\FilesystemLoader('view');
+    $twig = new \Twig\Environment($loader, [
+    'cache' => false,
+    ]);
+    $controller = new Controller();
+    $controller->partinscription($twig);
+});
+Route::get('/proinscription', function(){
+    $loader = new \Twig\Loader\FilesystemLoader('view');
+    $twig = new \Twig\Environment($loader, [
+    'cache' => false,
+    ]);
+    $controller = new Controller();
+    $controller->proinscription($twig);
+});
+Route::get('/kidsinscription', function(){
+    $loader = new \Twig\Loader\FilesystemLoader('view');
+    $twig = new \Twig\Environment($loader, [
+    'cache' => false,
+    ]);
+    $controller = new Controller();
+    $controller->kidsinscription($twig);
+});
+Route::get('/', function(){
+    $loader = new \Twig\Loader\FilesystemLoader('view');
+    $twig = new \Twig\Environment($loader, [
+    'cache' => false,
+    ]);
+    $controller = new Controller();
+    $controller->accueil($twig);
+});
+Route::get('/moncompte', function(){
+    $loader = new \Twig\Loader\FilesystemLoader('view');
+    $twig = new \Twig\Environment($loader, [
+    'cache' => false,
+    ]);
+    $controller = new Controller();
+    $controller->moncompte($twig);
+});
+Route::get('/informations', function(){
+    $loader = new \Twig\Loader\FilesystemLoader('view');
+    $twig = new \Twig\Environment($loader, [
+    'cache' => false,
+    ]);
+    $controller = new Controller();
+    $controller->informations($twig);
+});
+Route::get('/localisation', function(){
+    $loader = new \Twig\Loader\FilesystemLoader('view');
+    $twig = new \Twig\Environment($loader, [
+    'cache' => false,
+    ]);
+    $controller = new Controller();
+    $controller->localisation($twig);
+});
+Route::get('/financement', function(){
+    $loader = new \Twig\Loader\FilesystemLoader('view');
+    $twig = new \Twig\Environment($loader, [
+    'cache' => false,
+    ]);
+    $controller = new Controller();
+    $controller->financement($twig);
 });
